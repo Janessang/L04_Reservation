0fc84ffe4cd44051f5e4b7b26f1dd6b9aace5943
Janessang <20025312@myrp.edu.sg>
2021-05-10 14:27:41 +0800

    changes made

--- app/src/main/java/sg/edu/rp/c346/id20025312/l04_reservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20025312/l04_reservation/MainActivity.java
@@ -3,16 +3,15 @@ package sg.edu.rp.c346.id20025312.l04_reservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
-import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
     DatePicker dp;
     TimePicker tp;
     Button btnReset;
-    Button btnCheck;
-    RadioButton sarea;
-    RadioButton nsarea;
+    Button btnReserve;
+    CheckBox sArea;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,28 +35,24 @@ public class MainActivity extends AppCompatActivity {
         cusName = findViewById(R.id.customerName);
         cusNum = findViewById(R.id.customerNum);
         size = findViewById(R.id.sizeOfGrp);
-        btnCheck = findViewById(R.id.buttonCheck);
+        btnReserve = findViewById(R.id.buttonReserve);
         btnReset = findViewById(R.id.buttonReset);
-        sarea = findViewById(R.id.sArea);
-        nsarea = findViewById(R.id.nsArea);
+        sArea = findViewById(R.id.sArea);
+        tvDisplay = findViewById(R.id.textViewDisplay);
 
-        btnCheck.setOnClickListener(new View.OnClickListener() {
+        btnReserve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String timeIs = "Time is " + tp.getCurrentHour() + ":" + tp.getCurrentMinute();
                 String dateIs = "Date is " + dp.getDayOfMonth() + "/" + dp.getMonth()+1 + "/" + dp.getYear();
-
-                if (sarea.isChecked()) {
-                    String area = sarea.toString();
-                    String display = String.format("Name: %s\nMobile Number: %d\nSize of Group: %d\n%s\n%s\nSeating Area:%s",cusName,cusNum,size,dateIs,timeIs,area);
-                    Toast.makeText(MainActivity.this, display, Toast.LENGTH_SHORT).show();
+                if (sArea.isChecked()) {
+                    tvDisplay.setText("Seating Area: Smoking Area");
 
                 } else {
-                    String area = nsarea.toString();
-                    String display = String.format("Name: %s\nMobile Number: %d\nSize of Group: %d\n%s\n%s\nSeating Area:%s",cusName,cusNum,size,dateIs,timeIs,area);
-                    Toast.makeText(MainActivity.this, display, Toast.LENGTH_SHORT).show();
-
+                    tvDisplay.setText("Seating Area: Non-Smoking Area");
                 }
+
+                tvDisplay.setText("Name: " + cusName + "\nMobile Number: " + cusNum + "\nSize of Group: " + size + "\n" + dateIs + "\n" + timeIs + "\nSeating Area: " + sArea);
             }
         });
 
@@ -66,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 tp.setCurrentHour(19);
                 tp.setCurrentMinute(30);
-                dp.updateDate(2020, 6,01);
+                dp.updateDate(2020, 5,01);
             }
         });
 
